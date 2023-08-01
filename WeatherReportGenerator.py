1e5e0402868842522a5c1d365a33f9bf9871d1cb
Chaitanya Vantaku <chaitanyavantaku@gmail.com>
2023-08-01 17:39:48 +0530

    Revert "Modified file"
    This reverts commit 8172d0ced9b682bdb0e9439e3668ca8946540c27.

--- WeatherReportGenerator.py
+++ WeatherReportGenerator.py
@@ -6,6 +6,12 @@ resp = requests.get(url, verify=False)
 
 listWeathers = resp.json()['list']
 
+print("1 for weather")
+print("2 for Wind Speed")
+print("3 for Pressure")
+print("0 for Exit")
+choice = input("Enter a choice")
+
 
 def get_Weather(date_inp):
     for i in listWeathers:
@@ -14,27 +20,19 @@ def get_Weather(date_inp):
     return False
 
 
-choice = 1
-
-while choice != "0":
-    print("1 for weather")
-    print("2 for Wind Speed")
-    print("3 for Pressure")
-    print("0 for Exit")
-    choice = input("Enter a choice")
-    if choice == "1":
-        date = input("Enter date in format(YYYY-MM-DD hh-mm-ss) :")
-        result = get_Weather(date)
-        if result != 0:
-            print("Main: " + result['weather'][0]['main'])
-            print("Description " + result['weather'][0]['description'])
-    elif choice == "2":
-        date = input("Enter date in format(YYYY-MM-DD hh-mm-ss) :")
-        result = get_Weather(date)
-        if result != 0:
-            print("Wind speed: " + str(result['wind']['speed']))
-    elif choice == "3":
-        date = input("Enter date in format(YYYY-MM-DD hh-mm-ss) :")
-        result = get_Weather(date)
-        if result != 0:
-            print("Pressure: " + str(result['main']['pressure']))
+if choice == "1":
+    date = input("Enter date in format(YYYY-MM-DD hh-mm-ss) :")
+    result = get_Weather(date)
+    if result != 0:
+        print("Main: "+result['weather'][0]['main'])
+        print("Description "+result['weather'][0]['description'])
+elif choice == "2":
+    date = input("Enter date in format(YYYY-MM-DD hh-mm-ss) :")
+    result = get_Weather(date)
+    if result != 0:
+        print("Wind speed: "+str(result['wind']['speed']))
+elif choice == "3":
+    date = input("Enter date in format(YYYY-MM-DD hh-mm-ss) :")
+    result = get_Weather(date)
+    if result != 0:
+        print("Pressure: "+str(result['main']['pressure']))
